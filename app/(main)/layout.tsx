6eeb1f15f1eb5f90c4813e8c57b7dffab7a4f6b4
mhmd-aashik <aashikdevelop@gmail.com>
2024-12-12 02:13:43 +0530

    Responsive sticky sidebar/bottom bar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,4 +1,5 @@
 import { validateRequest } from "@/auth";
+import MenuBar from "@/components/menu/Menubar";
 import Navbar from "@/components/navbar/Navbar";
 import SessionProvider from "@/context/SessionProvider";
 import { redirect } from "next/navigation";
@@ -13,7 +14,11 @@ export default async function Layout({
     <SessionProvider value={session}>
       <div className="flex min-h-screen flex-col">
         <Navbar />
-        <div className="mx-auto max-w-7xl p-5">{children}</div>
+        <div className="mx-auto flex w-full max-w-7xl grow gap-5 p-5">
+          <MenuBar className="sticky top-[5.25rem] hidden h-fit flex-none space-y-3 rounded-2xl bg-card px-3 py-5 shadow-sm sm:block lg:px-5 xl:w-80" />
+          {children}
+        </div>
+        <MenuBar className="sticky bottom-0 flex w-full justify-center gap-5 border-t bg-card p-3 sm:hidden" />
       </div>
     </SessionProvider>
   );
